4ef6aa684f4d251b0a6fa704fef66f2d4f76afdc
erdzan12 <jungerjunge2@gmail.com>
2021-03-24 01:37:37 +0000

    update

--- src/main/i5/las2peer/services/poemstwo/Poemstwo.java
+++ src/main/i5/las2peer/services/poemstwo/Poemstwo.java
@@ -71,10 +71,10 @@ public class Poemstwo extends RESTService {
 
   @Api
   @SwaggerDefinition(
-      info = @Info(title = "12", version = "$Metadata_Version$",
-          description = "$Metadata_Description$",
-          termsOfService = "$Metadata_Terms$",
-          contact = @Contact(name = "", email = "CAEAddress@gmail.com") ,
+      info = @Info(title = "12", version = "",
+          description = "",
+          termsOfService = "",
+          contact = @Contact(name = "mee", email = "CAEAddress@gmail.com") ,
           license = @License(name = "BSD",
               url = "https://github.com/CAETESTRWTH/microservice-12/blob/master/LICENSE.txt") ) )
   @Path("/")
